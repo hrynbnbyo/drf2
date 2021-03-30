6b2c8c6fbfa3eed24120c742c640b16ec4a62985
hrynbnbyo <81204198+hrynbnbyo@users.noreply.github.com>
2021-03-31 03:52:44 +0800

    Update urls.py

--- day2/urls.py
+++ day2/urls.py
@@ -5,6 +5,6 @@ from day2 import views
 urlpatterns = [
     path("users/", views.UserAPIView.as_view()),
     path("users/<str:id>/", views.UserAPIView.as_view()),
-    path("employee/", views.StudentAPIView.as_view()),
-    path("employee/<str:id>/", views.StudentAPIView.as_view()),
+    path("student/", views.StudentAPIView.as_view()),
+    path("student/<str:id>/", views.StudentAPIView.as_view()),
 ]
